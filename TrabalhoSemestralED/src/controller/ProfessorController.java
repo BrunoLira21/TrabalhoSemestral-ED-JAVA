4dcad20128aed7c678321230df608d32742902ab
georgios <gabriel.andrewbg@gmail.com>
2025-11-20 19:44:56 -0300

    Adição de funcionalidade no botão adicionar Curso, controller do professor e ajustes no controller curso

--- TrabalhoSemestralED/src/controller/ProfessorController.java
+++ TrabalhoSemestralED/src/controller/ProfessorController.java
@@ -1,13 +1,58 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package controller;
 
-/**
- *
- * @author Família Lira
- */
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import entity.Professor;
+
+import javax.swing.*;
+
 public class ProfessorController {
-    
+    private String separador = ";";
+    private String caminho = "Arquivos/professor.csv";
+
+    public void adicionarProfessor(Professor prof) throws Exception {
+
+        if (!exists(prof.getCpf())) {
+            try (BufferedWriter escrever = new BufferedWriter(new FileWriter(caminho, true))) {
+                String linha = prof.getCpf() + separador +
+                        prof.getNome() + separador +
+                        prof.getArea() + separador +
+                        prof.getPonto() + separador;
+                escrever.write(linha);
+                escrever.newLine();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        } else {
+            JOptionPane.showMessageDialog(null, "O CPF inserido já está cadastrado",
+                    "Cadastro do Professor", JOptionPane.INFORMATION_MESSAGE);
+        }
+    }
+
+    public void removerProfessor(Professor prof) throws Exception {
+
+    }
+
+    public void consultarProfessor() throws Exception {
+
+    }
+
+    private boolean exists(String cpfProfessor) throws Exception {
+        BufferedReader ler = new BufferedReader(new FileReader(caminho));
+        String linha;
+        while ((linha = ler.readLine()) != null) {
+            String[] colunas = linha.split(separador);
+
+            if (colunas.length == 4) {
+                String cpf = colunas[0].trim();
+
+                if (cpf.equals(cpfProfessor)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
 }
